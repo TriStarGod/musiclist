d4a9839e6d60edd39d17ad20ae5468f2a6f046b2
Dinesh A <dfa2222@gmail.com>
2017-08-04 11:53:02 -0400

    new develop branch

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // loading webserver modules
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+// const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 // route files when a client requests a page
-var index = require('./routes/index');
+const index = require('./routes/index');
+const api = require('./routes/api/index');
 // instances of express server
-var app = express();
+const app = express();
 
 // view engine setup - where its found and use view engine ejs
 app.set('views', path.join(__dirname, 'views'));
@@ -25,16 +26,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // which route files to use
 app.use('/', index);
-
+app.use('/api', api);
 // catch 404 and forward to error handler; inline middleware;
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,21 @@ const router = express.Router();
 
 // GET, POST, PUT, DELETE
 /* GET home page. */
+// for path /, req (request), res (result), next
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'MusicList' });
+  res.render('index', { title: 'MusicList Alpha' });
 });
-const testJSON = [
-  {
-    name: 'John Smith',
-    username: 'Cupcake'
-  },
-  {
-    name: 'Jane Doe',
-    username: 'Stinky'
-  }
-];
-router.get('/sendjson', (req, res, next) => {
-  res.json(testJSON);
-});
+// const testJSON = [
+//   {
+//     name: 'John Smith',
+//     username: 'Cupcake'
+//   },
+//   {
+//     name: 'Jane Doe',
+//     username: 'Stinky'
+//   }
+// ];
+// router.get('/sendjson', (req, res, next) => {
+//   res.json(testJSON);
+// });
 module.exports = router;
